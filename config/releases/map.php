5d98ef9323db7b91f27eacce94df9eebc27155d4
dogmatic69 <dogmatic69@gmail.com>
2011-07-25 23:48:13 +0100

    adding a release for the twitter plugin

---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000008_twitter' => 'R4e2df29a456443c981b13f406318cd70'),
+);
+?>
---
+++ config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* Twitter schema generated on: 2011-07-25 23:07:54 : 1311634074*/
+class TwitterSchema extends CakeSchema {
+	var $name = 'Twitter';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
